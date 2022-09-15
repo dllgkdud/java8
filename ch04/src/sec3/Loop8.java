f8a813b8177e24f647a85b0ae3aabb415d7a2940
dllgkdud <dllgkdud@gmail.com>
2022-09-15 17:23:01 +0900

    java condition and looping test

--- ch04/src/sec3/Loop8.java
+++ ch04/src/sec3/Loop8.java
@@ -10,11 +10,22 @@ public class Loop8 {
   	...
   	2*9=18	3*9=27	... 9*9=81
  */
-		int pro=2;
-		for(int a=1;a<10;a++){
-			pro*=a;
+		for(int a=1;a<=9;a++){
+			for(int b=2;b<=9;b++){
+				System.out.print(b+"*"+a+"="+(a*b)+"\t");
+				
+			} System.out.println("\n");
 		}
-		System.out.println(pro);
+		System.out.println("\n");
+		System.out.println("수열 : 팩토리얼의 합계 출력");
+		//결과: 1!+2!+3!+4!+5!+6!+7!+8!+9!+10!
+		int pac=1;
+		long res=0;
+		for (int x=1;x<=10;x++){
+			pac*=x;	//1!~10!까지 각각의 값
+			res+=pac;	//총 합계
+		}
+		System.out.println("1!+2!+...+10! = "+res);
 	}
 
 }
